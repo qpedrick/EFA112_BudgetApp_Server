f06eb107759a3709965d726e234d370fb534e63a
Kiersten Gamby <kierstengamby@gmail.com>
2021-12-10 11:49:00 -0500

    all updates complete - working on debugging user login in postman

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const { UserModel } = require("../models");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const { UniqueConstraintError } = require("sequelize/lib/errors");
+const { UniqueConstraintError } = require("sequelize/dist");
 
 router.post("/register", async (req, res) => {
     let { email, password } = req.body.user;
--- models/index.js
+++ models/index.js
@@ -1,11 +1,9 @@
 const UserModel = require('./user');
-const BudgetModel = require('./budget')
 const IncomeModel = require('./income')
 const ExpenseModel = require('./expense')
 
 module.exports = { 
     UserModel,
-    BudgetModel,
     IncomeModel,
     ExpenseModel
 };
